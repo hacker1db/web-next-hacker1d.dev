653c13f4580782b8c58968eaf71af4d40dd98a00
hacker1db <hacker1db@outlook.com>
2023-03-12 15:14:30 -0700

    Added github folder and such

--- app/content/[slug]/page.tsx
+++ app/content/[slug]/page.tsx
@@ -1,21 +1,23 @@
 import fs from "fs";
 import Markdown from "markdown-to-jsx";
+import matter from "gray-matter";
 
 const getBlogPostContent = (slug: string) => {
   const blogPostsDir = "content/";
   const file = `${blogPostsDir}${slug}.md`;
   const blogPostContent = fs.readFileSync(file, "utf8");
+  const matterContent = matter(blogPostContent);
 
-  return blogPostContent;
+  return matterContent;
 };
 
 const PostPage = (props: any) => {
   const slug = props.params.slug;
-  const content = getBlogPostContent(slug);
+  const post = getBlogPostContent(slug);
   return (
     <div>
-      <h1>{slug}</h1>
-      <Markdown>{content}</Markdown>, document.body)
+      <h1>{post.data.title}</h1>
+      <Markdown>{post.content}</Markdown>, document.body)
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import fs from "fs";
 import Link from "next/link";
+import matter from "gray-matter";
+import { PostMetadata } from "../models/PostMetadataModels";
 
 function getBlogPosts(): string[] {
   const blogPostsDir = "content/";
@@ -10,13 +12,35 @@ function getBlogPosts(): string[] {
   return slugs;
 }
 
+const getPostMetadata = (): PostMetadata[] => {
+  const folder = "content/";
+  const files = fs.readdirSync(folder);
+  const markdownPosts = files.filter((file) => file.endsWith(".md"));
+
+  // Get gray-matter data from each file.
+  const posts = markdownPosts.map((fileName) => {
+    const fileContents = fs.readFileSync(`content/${fileName}`, "utf8");
+    const matterResult = matter(fileContents);
+    return {
+      title: matterResult.data.title,
+      date: matterResult.data.date,
+      subtitle: matterResult.data.subtitle,
+      slug: fileName.replace(".md", ""),
+    };
+  });
+
+  return posts;
+};
+
 const HomePage = () => {
-  const contentData = getBlogPosts();
-  const postsPreview = contentData.map((slug, index) => (
+  const contentData = getPostMetadata();
+  const postsPreview = contentData.map((post, index) => (
     <div key={index}>
-      <Link href={`/content/${slug}`}>
-        <h2>{slug}</h2>
+      <Link href={`/content/${post.slug}`}>
+        <h2>{post.title}</h2>
       </Link>
+      <p>{post.subtitle}</p>
+      <p> {post.date}</p>
     </div>
   ));
 
